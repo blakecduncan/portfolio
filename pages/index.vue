9c2bceffbcb552030f42d22572e63db14dc9753d
Blake Duncan <blake@thriva.co>
2022-05-02 14:53:57 +0100

    Add a temporary home page

---
+++ pages/index.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="bg-gradient-to-r from-red-400 via-green-600 to-cyan-400 font-mono bg-white relative overflow-hidden h-screen">
+    <header class="h-24 sm:h-32 flex items-center z-30 w-full">
+      <div class="container mx-auto px-6 flex items-center justify-between">
+        <div class="uppercase text-gray-800 dark:text-white font-black text-3xl flex items-center">
+          <svg width="25" height="25" viewBox="0 0 1792 1792" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+            <path d="M1664 1504v-768q-32 36-69 66-268 206-426 338-51 43-83 67t-86.5 48.5-102.5 24.5h-2q-48 0-102.5-24.5t-86.5-48.5-83-67q-158-132-426-338-37-30-69-66v768q0 13 9.5 22.5t22.5 9.5h1472q13 0 22.5-9.5t9.5-22.5zm0-1051v-24.5l-.5-13-3-12.5-5.5-9-9-7.5-14-2.5h-1472q-13 0-22.5 9.5t-9.5 22.5q0 168 147 284 193 152 401 317 6 5 35 29.5t46 37.5 44.5 31.5 50.5 27.5 43 9h2q20 0 43-9t50.5-27.5 44.5-31.5 46-37.5 35-29.5q208-165 401-317 54-43 100.5-115.5t46.5-131.5zm128-37v1088q0 66-47 113t-113 47h-1472q-66 0-113-47t-47-113v-1088q0-66 47-113t113-47h1472q66 0 113 47t47 113z">
+            </path>
+          </svg>
+          <span class="text-xs ml-3 mt-1">
+            blakecduncan@gmail.com
+          </span>
+        </div>
+        <div class="flex items-center">
+          <nav class="font-sen text-gray-800 dark:text-white uppercase text-lg lg:flex items-center hidden">
+            <a href="#" class="py-2 px-6 flex hover:text-black">
+              Work
+            </a>
+            <a href="#" class="py-2 px-6 flex hover:text-black">
+              Resume
+            </a>
+          </nav>
+          <button class="lg:hidden flex flex-col ml-4">
+            <span class="w-6 h-1 bg-gray-800 dark:bg-white mb-1" />
+            <span class="w-6 h-1 bg-gray-800 dark:bg-white mb-1" />
+            <span class="w-6 h-1 bg-gray-800 dark:bg-white mb-1" />
+          </button>
+        </div>
+      </div>
+    </header>
+    <div class="flex relative z-20 items-center">
+      <div class="container mx-auto px-6 flex flex-col justify-between items-center relative py-4">
+        <div class="flex flex-col">
+          <p class="text-3xl my-6 text-center dark:text-white">
+            Hi, I&#x27;m Blake 🤘
+          </p>
+          <h2 class="max-w-3xl text-5xl md:text-6xl font-bold mx-auto dark:text-white text-gray-800 text-center py-2">
+            I&#x27;m an experienced full-stack software engineer
+          </h2>
+          <div class="flex items-center justify-center mt-4">
+            <a href="https://www.linkedin.com/in/blake-c-duncan/" class="bg-blue-600 p-2 font-semibold text-white inline-flex items-center space-x-2 rounded mr-2">
+              <svg class="w-5 h-5 fill-current" role="img" viewBox="0 0 256 256" xmlns="http://www.w3.org/2000/svg">
+                <g><path d="M218.123122,218.127392 L180.191928,218.127392 L180.191928,158.724263 C180.191928,144.559023 179.939053,126.323993 160.463756,126.323993 C140.707926,126.323993 137.685284,141.757585 137.685284,157.692986 L137.685284,218.123441 L99.7540894,218.123441 L99.7540894,95.9665207 L136.168036,95.9665207 L136.168036,112.660562 L136.677736,112.660562 C144.102746,99.9650027 157.908637,92.3824528 172.605689,92.9280076 C211.050535,92.9280076 218.138927,118.216023 218.138927,151.114151 L218.123122,218.127392 Z M56.9550587,79.2685282 C44.7981969,79.2707099 34.9413443,69.4171797 34.9391618,57.260052 C34.93698,45.1029244 44.7902948,35.2458562 56.9471566,35.2436736 C69.1040185,35.2414916 78.9608713,45.0950217 78.963054,57.2521493 C78.9641017,63.090208 76.6459976,68.6895714 72.5186979,72.8184433 C68.3913982,76.9473153 62.7929898,79.26748 56.9550587,79.2685282 M75.9206558,218.127392 L37.94995,218.127392 L37.94995,95.9665207 L75.9206558,95.9665207 L75.9206558,218.127392 Z M237.033403,0.0182577091 L18.8895249,0.0182577091 C8.57959469,-0.0980923971 0.124827038,8.16056231 -0.001,18.4706066 L-0.001,237.524091 C0.120519052,247.839103 8.57460631,256.105934 18.8895249,255.9977 L237.033403,255.9977 C247.368728,256.125818 255.855922,247.859464 255.999,237.524091 L255.999,18.4548016 C255.851624,8.12438979 247.363742,-0.133792868 237.033403,0.000790807055"></path></g>
+              </svg>
+            </a>
+
+            <a href="https://github.com/blakecduncan" class="bg-gray-700 p-2 font-semibold text-white inline-flex items-center space-x-2 rounded">
+              <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" class="w-5" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24">
+                <g fill="none"><path fill-rule="evenodd" clip-rule="evenodd" d="M12 0C5.37 0 0 5.37 0 12c0 5.31 3.435 9.795 8.205 11.385c.6.105.825-.255.825-.57c0-.285-.015-1.23-.015-2.235c-3.015.555-3.795-.735-4.035-1.41c-.135-.345-.72-1.41-1.23-1.695c-.42-.225-1.02-.78-.015-.795c.945-.015 1.62.87 1.845 1.23c1.08 1.815 2.805 1.305 3.495.99c.105-.78.42-1.305.765-1.605c-2.67-.3-5.46-1.335-5.46-5.925c0-1.305.465-2.385 1.23-3.225c-.12-.3-.54-1.53.12-3.18c0 0 1.005-.315 3.3 1.23c.96-.27 1.98-.405 3-.405s2.04.135 3 .405c2.295-1.56 3.3-1.23 3.3-1.23c.66 1.65.24 2.88.12 3.18c.765.84 1.23 1.905 1.23 3.225c0 4.605-2.805 5.625-5.475 5.925c.435.375.81 1.095.81 2.22c0 1.605-.015 2.895-.015 3.3c0 .315.225.69.825.57A12.02 12.02 0 0 0 24 12c0-6.63-5.37-12-12-12z" fill="currentColor" /></g>
+              </svg>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  head: {
+    title: 'Blake Duncan',
+    meta: [
+      {
+        hid: 'blake duncan portfolio site',
+        name: 'blake duncan portfolio site',
+        content: 'Blake Duncan Home'
+      }
+    ],
+  }
+};
+</script>
