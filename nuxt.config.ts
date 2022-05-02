accdcdafa5a2aaae513f5e9b654291e0a928fed7
Blake Duncan <blake@thriva.co>
2022-05-02 13:51:22 +0100

    Add tailwind support

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,16 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
+    typescript: {
+        strict: true
+    },
+    modules: [
+        '@nuxtjs/tailwindcss'
+    ],
+    postcss: {
+        plugins: {
+            tailwindcss: {},
+            autoprefixer: {},
+        },
+    },
 })
